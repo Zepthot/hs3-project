a4b88446dbd65db042dd924668383ef2e7ce1d48
Zepthot <zepthot@gmail.com>
2023-09-05 15:55:35 +0200

    initial commit

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../../styles/home.scss';
+
+// Core function with all sections
+function Core() {
+  return (
+    <React.StrictMode>
+      <h1>Home</h1>
+    </React.StrictMode>
+  );
+}
+
+// Export to call it up in index.js
+export default Core;
